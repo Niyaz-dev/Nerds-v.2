b48ce07ea6a75386bcca8750c16f97325944afc5
Niyaz <niyaz_06@mail.ru>
2020-09-29 19:33:07 +0300

    adaptive: finished filters

--- src/js/filters.js
+++ src/js/filters.js
@@ -1,7 +1,7 @@
-f
-g
-h
-f
-j
-g
-h
+let filtersButton = document.querySelector('.catalog__show-filters');
+let filters = document.querySelector('.filters');
+
+filtersButton.addEventListener('click',() => {
+    filters.classList.toggle('show-filter');
+})
+
