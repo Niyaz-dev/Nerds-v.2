d3236c6323abc713f19d24e7c462ea5348820940
Niyaz <niyaz_06@mail.ru>
2020-09-30 19:32:11 +0300

    adaptive: menu for mobile version

---
+++ src/js/navigation-toggle.js
@@ -0,0 +1,13 @@
+var navMain = document.querySelector('.header__navigation');
+var navToggle = document.querySelector('.header__toggle');
+
+navToggle.addEventListener('click',function(){
+    if(navMain.classList.contains('header__navigation--closed')){
+        navMain.classList.remove('header__navigation--closed');
+        navMain.classList.add('header__navigation--opened');
+    }
+    else{
+        navMain.classList.add('header__navigation--closed');
+        navMain.classList.remove('header__navigation--opened');
+    }
+});
